62e651ecc78532a5d703560e57a991bbef0ecc9c
Gor Lalayan <lalayan.gor.1@mail.ru>
2024-04-27 11:34:34 +0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+const jsxElement =(
+  <h1 className='word' >Hello</h1>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(jsxElement)
