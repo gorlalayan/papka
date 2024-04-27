54565cbfa0959845f2b6409454bbba134df5c1ba
Gor Lalayan <lalayan.gor.1@mail.ru>
2024-04-27 11:56:36 +0400

    add file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import Word from './word/word'
 
 
--- src/word/word.jsx
+++ src/word/word.jsx
@@ -1,5 +1,6 @@
 import './index.css'
+
 const Word=()=>{
-    <h1>HELLO</h1>
+    <h1 className='word'>HELLO</h1>
 }
 export default Word
